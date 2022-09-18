8e1e9db035f0860586613a05f26c498cb2ca1fad
sovaos42 <sovaos42@gmail.com>
2022-09-18 23:56:21 +0700

    Initial commit

---
+++ locallibrary/locallibrary/catalog/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+from django.urls import re_path
+
+urlpatterns = [
+    re_path(r'^$', views.index, name='index'),
+    re_path(r'^books/$', views.BookListView.as_view(), name='books'),
+    re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
+    re_path(r'^author/$', views.AuthorListView.as_view(), name='author'),
+    re_path(r'^author/(?P<pk>\d+)$', views.AuthorDetailView.as_view(), name='author-detail'),
+    re_path(r'^mybooks/$', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    re_path(r'^borrowed/$', views.LoanedBooksAllListView.as_view(), name='all-borrowed'),
+]
